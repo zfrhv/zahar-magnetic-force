2cefe561fc5074df930ffa8e6629ca99c6c10b78
unknown <zfrhv2010@gmail.com>
2022-09-23 15:27:35 +0300

    a

--- public/sidebar/sidebar.js
+++ public/sidebar/sidebar.js
@@ -8,17 +8,28 @@ window.addEventListener('click', function (e) {
   }
 })
 
-const sidebar = document.createElement("div");
+const sidebar = document.createElement("div")
+sidebar.id = "SideBar"
+
 sidebar.innerHTML = `
 <span id="myMenu" onclick="openNav()">&#9776;</span>
 <div id="mySidenav" class="sidenav">
   <link rel="stylesheet" type="text/css" href="sidebar/sidebar.css" />
   <a href="physics.html">Main Page</a>
   <a href="discovering_equation.html">Discovering Equation</a>
-  <a href="comparing_equations.html">Comparing Equations</a>
+  <a href="sensing_equation.html">Sensing Equation</a>
   <a href="discussion.html">Discussion</a>
 </div>
 `
 document.getElementById("SideBar").replaceWith(sidebar)
 
-openNav()
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+}else{
+  // false for not mobile device
+  openNav()
+}
+
+const selected = sidebar.querySelector('[href="'+window.location.pathname.match(/[^/]+$/g)[0]+'"]')
+selected.style.backgroundColor = "#383636"
+selected.style.color = "#d6d5d2"
